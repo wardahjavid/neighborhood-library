4e37ab83b4e1ce2d996ef5e6297437f30903f7aa
Student <wardahjavid62@gmail.com>
2025-10-06 09:47:21 -0400

    second commit

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -86,11 +86,12 @@ public class Book {
         return id + ", " + isbn + ", " + title + ", Checked out to " + checkedOutTo;
     }
 
-    static class neighorhoodLibrary() {
+    static class NeighorhoodLibrary() {
         public static void main(String[] args) {
-            Scanner input = new Scanner(System.in);
-            Book[] book = new Book[20];
-            book[0] = new Book(1, "9876543211234", "The Kite Runner");
+            Scanner potatoscanner = new Scanner(System.in);
+
+            Book[] book = new Book[24];
+            book[0] = new Book(1, "9876543211234", "Endgame: The Betrayal and Fall of Srebrenica, Europe's Worst Massacre Since World War II");
             book[1] = new Book(2, "9876543215678", "Mornings in Jenin");
             book[2] = new Book(3, "9876543219123", "Return to Haifa");
             book[3] = new Book(4, "9876543218123", "Against the Loveless World");
@@ -109,11 +110,43 @@ public class Book {
             book[16] = new Book(17, "987654321387", "Speak, Silence");
             book[17] = new Book(18, "987654321287", "A Novel about the Balkans");
             book[18] = new Book(19, "987654321187", "The War Reporter");
-            book[20] = new Book(20, "987654321876", "The Wolf of Sarajevo");
-            book[21] = new Book(21, "987654321776", "A Love For A Country: A Story About Revenge");
-            book[22] = new Book(22, "987654321676", "They Are Trying To Break Your Heart");
-            book[21] = new Book(23, "987654321576", "Endgame: The Betrayal and Fall of Srebrenica, Europe's Worst Massacre Since World War II");
-            book[22] = new Book(24, "987654321476", "The Other Side of My Pillow");
+            book[19] = new Book(20, "987654321876", "The Wolf of Sarajevo");
+            book[20] = new Book(21, "987654321776", "A Love For A Country: A Story About Revenge");
+            book[21] = new Book(22, "987654321676", "They Are Trying To Break Your Heart");
+            book[22] = new Book(23, "987654321576", "The Kite Runner");
+            book[23] = new Book(24, "987654321476", "The Other Side of My Pillow");
+
+            boolean bookInformationRunning = true;
+
+            System.out.println("Welcome to the Neighborhood Library!");
+
+            while(bookInformationRunning) {
+                System.out.println("Home Screen");
+                System.out.println("1 - Please show all available books.");
+                System.out.println("2 - Please show all checked out books.");
+                System.out.println("3 - You may exit.");
+                System.out.println("4 - Please enter a choice from 1 through 3. ");
+                String selectOption = input.nextLine();
+
+                if (bookInformationRunning.equals("1")) {
+                    showBook(input, book);
+                } else if (bookInformationRunning.equals("2")) {
+                    showBook(input, book);
+                } else if (bookInformationRunning.equals("3")) {
+                    System.out.println("Exit. Goodbye!");
+                    break;
+                } else {
+                    System.out.println("This is an invalid choice. Please try again!");
+
+                }
+
+            }
+            potatoscanner.close();
+
+            }
+
+
+
 
         }
     }
